766d91298b05790f87286cecfcb46dd8b27eb1f1
cottons-kr <devkim@hanarin.uk>
2024-05-11 18:45:03 +0900

    Remove unused code

--- app/src/main/java/com/example/tapie_weather/ui/theme/Color.kt
+++ app/src/main/java/com/example/tapie_weather/ui/theme/Color.kt
@@ -2,8 +2,6 @@ package com.example.tapie_weather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Gray = Color(0xFFA0A0A0)
-
 val BackgroundColor = Color(0xFF080811)
 val PrimaryColor = Color(0xFF1273FE)
 val SectionColor = Color(0xFF18171C)
